be57c432d6958858fb5745a12442700cae425480
Young Kim <ykim320@gmail.com>
2011-10-08 22:06:49 -0400

    Updated AnalysisActivity.java CommandLineUtil.java PingHelper.java and tested works for now

--- client/Ping/src/com/android/helpers/PingHelper.java
+++ client/Ping/src/com/android/helpers/PingHelper.java
@@ -6,21 +6,23 @@ import com.android.utils.CommandLineUtil;
 public class PingHelper {
 	
 	public static CommandLineUtil cmdUtil;
-	public static String ip_address;
+	public static String ip_address = "localhost";
+	public String pingOutput;
 	
 	/**
 	 * PingHelp helps run ping command by creating cmd and inputs
 	 * @param p - Ping object
 	 * @return
 	 */
-	private Ping pingHelp(Ping p)
+	public Ping pingHelp(Ping p)
 	{
 		String ip 		= p.getDstIp();
 		String cmd 		= "ping";
 		String options 	= "-c 5";
 		String output 	= "";
-		
+		cmdUtil = new CommandLineUtil();
 		output = cmdUtil.runCommand(cmd, ip, options);
+		pingOutput = output;
 		// Parse output and edit p
 		// p.~~~~
 		return p;
@@ -30,18 +32,28 @@ public class PingHelper {
 	 * Pinghelp helps run ping command by creating cmd and inputs
 	 * @return
 	 */
-	private Ping pingHelp()
+	public Ping pingHelp()
 	{
 		Ping p 			= null;
 		String ip 		= ip_address;
 		String cmd 		= "ping";
 		String options 	= "-c 5";
 		String output 	= "";
-		
+
+		cmdUtil = new CommandLineUtil();
 		output = cmdUtil.runCommand(cmd, ip, options);
+		pingOutput = output;
 		// Parse output and edit p
 		// p.~~~~
 		return null;
+	}
+
+	public String getPingOutput() {
+		return pingOutput;
+	}
+
+	public void setPingOutput(String pingOutput) {
+		this.pingOutput = pingOutput;
 	}
 	
 }
