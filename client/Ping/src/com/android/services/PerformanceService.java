255d74709ca5e88eb5967ed07a364c5436a69999
Young Kim <ykim320@gmail.com>
2011-11-08 02:26:38 -0500

    Added Service and Edited Manifest

--- client/Ping/src/com/android/services/PerformanceService.java
+++ client/Ping/src/com/android/services/PerformanceService.java
@@ -5,10 +5,15 @@ import java.util.HashMap;
 import android.app.Activity;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.widget.TextView;
 
 import com.android.Session;
 import com.android.helpers.ServerHelper;
+import com.android.listeners.BaseResponseListener;
+import com.android.models.Ping;
 import com.android.tasks.MeasurementTask;
 
 public class PerformanceService extends Service{
@@ -27,4 +32,29 @@ public class PerformanceService extends Service{
 	}
 	
 
+	private void runTask() {
+		serverhelper.execute(new MeasurementTask(activity,new HashMap<String,String>(), new MeasurementListener()));
+	}
+	
+	private class MeasurementListener extends BaseResponseListener{
+
+		public void onCompletePing(Ping response) {
+			Message msg=Message.obtain(handler, 0, response);
+			handler.sendMessage(msg);		
+		}
+
+		public void onComplete(String response) {
+		
+		}
+	}
+
+	private Handler handler = new Handler() {
+		public void  handleMessage(Message msg) {
+			try {
+				Ping p=(Ping)msg.obj;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	};
 }
