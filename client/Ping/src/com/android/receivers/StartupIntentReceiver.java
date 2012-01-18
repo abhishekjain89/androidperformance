9f83599400f43a3f9dc13c0407a1e687947ad67a
Abhishek Jain <abhishekjain@gatech.edu>
2012-01-18 14:29:14 -0500

    added StartupReceiver

---
+++ client/Ping/src/com/android/receivers/StartupIntentReceiver.java
@@ -0,0 +1,17 @@
+package com.android.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class StartupIntentReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+	/*	Intent serviceIntent = new Intent();
+		serviceIntent.setAction("com.android.services.PerformanceService");
+		context.startService(serviceIntent); */
+		
+	}
+	
+}
