331a318618c390847e5fbed4a72f426be8990b7c
Young Kim <ykim320@gmail.com>
2012-01-23 01:51:48 -0500

    Removed redundant codes

--- client/Ping/src/com/android/helpers/DeviceHelper.java
+++ client/Ping/src/com/android/helpers/DeviceHelper.java
@@ -12,64 +12,20 @@ public class DeviceHelper {
 	
 	public static Measurement deviceHelp(Context context){
 		
-		//String phoneDetail = runPhoneDetail(context);
-		//String networkDetail = runNetworkDetail(context);
-		//return new Device(phoneDetail,networkDetail);
-		
 		return runFullDetail(context);
 	}
 	
 	public static Measurement runFullDetail(Context context) {
-		/*
-		DeviceUtil deviceUtil = new DeviceUtil();
-		GPSHelper gpsH = new GPSHelper();
-		Info dev = new Info();
-		dev = deviceUtil.getFullDetail(context);
-		GPS gps = gpsH.getGps(context);
-		dev.setAltitude(gps.getAltitude());
-		dev.setLatitude(gps.getLatitude());
-		dev.setLongitude(gps.getLongitude());
-		return dev;
-		*/
+
 		DeviceUtil deviceUtil = new DeviceUtil();
 		GPSHelper gpsH = new GPSHelper();
 		Measurement info = new Measurement();
-		GPS gps = gpsH.getGps(context);
 		info = deviceUtil.getFullDetail(context);
-//		if (gps.getLatitude() == ("Not Found"))	{
-//			info.setAltitude(GPS.getOldAltitude());
-//			info.setLatitude(GPS.getOldLatitude());
-//			info.setLongitude(GPS.getOldLongitude());
-//		}
-//		else {
-			info.setAltitude(gps.getAltitude());
-			info.setLatitude(gps.getLatitude());
-			info.setLongitude(gps.getLongitude());
-//		}
+		GPS gps = gpsH.getGps(context);
+		info.setAltitude(gps.getAltitude());
+		info.setLatitude(gps.getLatitude());
+		info.setLongitude(gps.getLongitude());
 		return info;
 		
 	}
-	
-	public static String runPhoneDetail(Context context) {
-		String srvnName = context.TELEPHONY_SERVICE;
-		TelephonyManager telephonyManager = (TelephonyManager)context.getSystemService(srvnName);
-		DeviceUtil deviceUtil = new DeviceUtil();
-		
-		return deviceUtil.getPhoneDetail(telephonyManager);
-		
-	}
-	
-	public static String runNetworkDetail(Context context) {
-		String srvnName = context.TELEPHONY_SERVICE;
-		TelephonyManager telephonyManager = (TelephonyManager)context.getSystemService(srvnName);
-		DeviceUtil deviceUtil = new DeviceUtil();
-		
-		String phoneDetail = deviceUtil.getNetworkDetail(telephonyManager);
-
-
-		phoneDetail += "\n" + deviceUtil.getConnectivityDetail(context);
-		
-		return phoneDetail;
-	}
-
 }
