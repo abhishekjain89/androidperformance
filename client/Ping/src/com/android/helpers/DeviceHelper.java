e9c37b5095d50b1f845fa33db266172ff14a52f8
Young Kim <ykim320@gmail.com>
2012-02-01 14:10:49 -0500

    temp

--- client/Ping/src/com/android/helpers/DeviceHelper.java
+++ client/Ping/src/com/android/helpers/DeviceHelper.java
@@ -17,20 +17,12 @@ public class DeviceHelper {
 	public static Measurement runFullDetail(Context context, Measurement info) {
 
 		DeviceUtil deviceUtil = new DeviceUtil();
-		GPSHelper gpsH = new GPSHelper();
 		info.setDevice(deviceUtil.getDeviceDetail(context));		
 		info.setNetwork(deviceUtil.getNetworkDetail(context));		
 		info.setSim(deviceUtil.getSimDetail(context));
-		info.setThroughput(null); // TO BE ADDED
-		info.setGps(null); // TO BE ADDED
 		info.setTime(deviceUtil.getTime());
 		info.setDeviceId(deviceUtil.getDeviceId(context));
 		
-		
-		//GPS gps = gpsH.getGps(context);
-		//info.setAltitude(gps.getAltitude());
-		//info.setLatitude(gps.getLatitude());
-		//info.setLongitude(gps.getLongitude());
 		return info;
 		
 	}
--- client/Ping/src/com/android/models/Measurement.java
+++ client/Ping/src/com/android/models/Measurement.java
@@ -22,42 +22,6 @@ public class Measurement {
 	String deviceId;
 /*-----------------------*/	
 	
-/*		MOVE THIS !	
-  -----------------------*/	
-	String phoneDetail;
-	String networkDetail;	
-	
-	// Device 
-	String phoneType;
-	String phoneNumber;
-	String softwareVersion;
-	
-	// Sim 
-	String simNetworkCountry;
-	String simState;
-	String simOperatorName;
-	String simOperatorCode;
-	String simSerialNumber;
-
-	// Network
-	String networkCountry;
-	String networkOperatorId;
-	String networkName;
-	String networkType;
-	String connectionType;
-	String mobileNetworkInfo;
-	String wifiState;
-	String cellId;
-	String cellLac;	
-	String dataState;
-	String dataActivity;
-
-	// GPS
-	String longitude;
-	String latitude;
-	String altitude;
-
-/*-------------------------*/	
 
 	public Device getDevice() {
 		return device;
@@ -90,31 +54,6 @@ public class Measurement {
 	public void setThroughput(Throughput throughput) {
 		this.throughput = throughput;
 	}
-	
-	public String getDataState() {
-		return dataState;
-	}
-
-	public void setDataState(String dataState) {
-		this.dataState = dataState;
-	}
-
-	public String getDataActivity() {
-		return dataActivity;
-	}
-
-	public void setDataActivity(String dataActivity) {
-		this.dataActivity = dataActivity;
-	}
-
-	public String getMobileNetworkInfo() {
-		return mobileNetworkInfo;
-	}
-
-	public void setMobileNetworkInfo(String mobileNetworkInfo) {
-		this.mobileNetworkInfo = mobileNetworkInfo;
-	}
-	
 	public String getTime() {
 		return time;
 	}
@@ -123,48 +62,8 @@ public class Measurement {
 		this.time = time;
 	}
 	
-	public String getCellId() {
-		return cellId;
-	}
-
-	public void setCellId(String cellId) {
-		this.cellId = cellId;
-	}
-
-	public String getCellLac() {
-		return cellLac;
-	}
-
-	public void setCellLac(String cellLac) {
-		this.cellLac = cellLac;
-	}
-
 	
 	
-	public String getPhoneType() {
-		return phoneType;
-	}
-
-	public void setPhoneType(String phoneType) {
-		this.phoneType = phoneType;
-	}
-
-	public String getPhoneDetail() {
-		return phoneDetail;
-	}
-
-	public void setPhoneDetail(String phoneDetail) {
-		this.phoneDetail = phoneDetail;
-	}
-
-	public String getNetworkDetail() {
-		return networkDetail;
-	}
-
-	public void setNetworkDetail(String networkDetail) {
-		this.networkDetail = networkDetail;
-	}
-
 	public String getDeviceId() {
 		return deviceId;
 	}
@@ -173,141 +72,6 @@ public class Measurement {
 		this.deviceId = deviceId;
 	}
 
-	public String getSoftwareVersion() {
-		return softwareVersion;
-	}
-
-	public void setSoftwareVersion(String softwareVersion) {
-		this.softwareVersion = softwareVersion;
-	}
-
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
-	public String getNetworkCountryISO() {
-		return networkCountry;
-	}
-
-	public void setNetworkCountryISO(String networkCountry) {
-		this.networkCountry = networkCountry;
-	}
-
-	public String getNetworkOperatorId() {
-		return networkOperatorId;
-	}
-
-	public void setNetworkOperatorId(String networkOperatorId) {
-		this.networkOperatorId = networkOperatorId;
-	}
-
-	public String getNetworkName() {
-		return networkName;
-	}
-
-	public void setNetworkName(String networkName) {
-		this.networkName = networkName;
-	}
-
-	public String getNetworkType() {
-		return networkType;
-	}
-
-	public void setNetworkType(String networkType) {
-		this.networkType = networkType;
-	}
-
-	public String getSimState() {
-		return simState;
-	}
-
-	public void setSimState(String simState) {
-		this.simState = simState;
-	}
-
-	public String getNetworkCountry() {
-		return networkCountry;
-	}
-
-	public void setNetworkCountry(String networkCountry) {
-		this.networkCountry = networkCountry;
-	}
-
-	public String getSimNetworkCountry() {
-		return simNetworkCountry;
-	}
-
-	public void setSimNetworkCountry(String simNetworkCountry) {
-		this.simNetworkCountry = simNetworkCountry;
-	}
-	
-	public String getSimOperatorCode() {
-		return simOperatorCode;
-	}
-
-	public void setSimOperatorCode(String simOperatorCode) {
-		this.simOperatorCode = simOperatorCode;
-	}
-
-	public String getSimOperatorName() {
-		return simOperatorName;
-	}
-
-	public void setSimOperatorName(String simOperatorName) {
-		this.simOperatorName = simOperatorName;
-	}
-
-	public String getSimSerialNumber() {
-		return simSerialNumber;
-	}
-
-	public void setSimSerialNumber(String simSerialNumber) {
-		this.simSerialNumber = simSerialNumber;
-	}
-
-	public String getConnectionType() {
-		return connectionType;
-	}
-
-	public void setConnectionType(String connectionType) {
-		this.connectionType = connectionType;
-	}
-
-	public String getWifiState() {
-		return wifiState;
-	}
-
-	public void setWifiState(String wifiState) {
-		this.wifiState = wifiState;
-	}
-
-	public String getLongitude() {
-		return longitude;
-	}
-
-	public void setLongitude(String longitude) {
-		this.longitude = longitude;
-	}
-
-	public String getLatitude() {
-		return latitude;
-	}
-
-	public void setLatitude(String latitude) {
-		this.latitude = latitude;
-	}
-
-	public String getAltitude() {
-		return altitude;
-	}
-
-	public void setAltitude(String altitude) {
-		this.altitude = altitude;
-	}
 	public ArrayList<Ping> getPings() {
 		return pings;
 	}
@@ -340,29 +104,8 @@ public class Measurement {
 				array.put(p.toJSON());
 			}
 			obj.putOpt("pings", array);
-			obj.putOpt("phoneNumber", phoneNumber);
 			obj.putOpt("deviceid", deviceId);
-			obj.putOpt("softwareVersion", softwareVersion);
-			obj.putOpt("networkCountry", networkCountry);
-			obj.putOpt("networkOperatorId", networkOperatorId);
-			obj.putOpt("networkName", networkName);
-			obj.putOpt("networkType", networkType);
-			obj.putOpt("simNetworkCountry", simNetworkCountry);
-			obj.putOpt("simState", simState);
-			obj.putOpt("simOperatorName", simOperatorName);
-			obj.putOpt("simOperatorCode", simOperatorCode);
-			obj.putOpt("simSerialNumber", simSerialNumber);
-			obj.putOpt("connectionType", connectionType);
-			obj.putOpt("mobileNetworkInfo", mobileNetworkInfo);
-			obj.putOpt("wifiState", wifiState);
-			obj.putOpt("longitude", longitude);
-			obj.putOpt("latitude", latitude);
-			obj.putOpt("altitude", altitude);
-			obj.putOpt("cellId", cellId);
-			obj.putOpt("cellLac", cellLac);		
-			obj.putOpt("time", time);			
-			obj.putOpt("dataState", dataState);
-			obj.putOpt("dataActivity", dataActivity);
+			obj.putOpt("time", time);	
 			
 			
 		} catch (JSONException e) {
