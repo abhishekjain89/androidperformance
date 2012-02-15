3ba780bcbb06058c0bc88cde906b274be2721976
Abhishek Jain <abhishekjain@gatech.edu>
2012-02-15 15:43:43 -0500

    forgot to re-add

---
+++ client/Ping/src/com/android/ui/viewgenerator/KeyValueViewGenerator.java
@@ -0,0 +1,38 @@
+package com.android.ui.viewgenerator;
+
+import com.android.R;
+import com.android.models.Model;
+import com.android.models.Row;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+public class KeyValueViewGenerator extends ViewGenerator{
+
+	ViewHolder holder;
+
+	public KeyValueViewGenerator(int resource) {
+		super(resource);
+		holder = new ViewHolder();
+	}
+
+	@Override
+	public ViewHolder fillViewHolder(View view,LayoutInflater inflater) {
+		
+		
+		holder.first =  (TextView) view.findViewById(R.id.key);
+		holder.second =  (TextView) view.findViewById(R.id.value);
+		
+		return holder;
+	}
+	@Override
+	public void populateView(Row item) {
+		holder.first.setText(item.first);
+		holder.second.setText(item.second);
+		
+	}
+	
+
+}
---
+++ client/Ping/src/com/android/ui/viewgenerator/ViewHolder.java
@@ -0,0 +1,10 @@
+package com.android.ui.viewgenerator;
+
+import android.widget.*;
+
+public class ViewHolder{
+		public TextView first;
+		public TextView second;
+		public ProgressBar progress;
+		public ImageView imageview;
+	}
